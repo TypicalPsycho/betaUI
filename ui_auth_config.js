794708b32e4fc6c575b53358c223046959d88608
TypicalPsycho <fastdad@live.com>
2026-03-16 12:53:30 -0500

    Avoids App.html flashing upon login for new users

--- ui_auth_config.js
+++ ui_auth_config.js
@@ -1,4 +1,4 @@
 // Public beta auth config. Safe for the browser.
 window.SB_SUPABASE_URL = window.SB_SUPABASE_URL || "https://tfrasnkqlfkdjahqlnrp.supabase.co";
 window.SB_SUPABASE_PUBLISHABLE_KEY = window.SB_SUPABASE_PUBLISHABLE_KEY || "sb_publishable_TjQ1NOXngHBZVHKVB-7NCA_LowdxCjA";
-window.SB_AUTH_REDIRECT_URL = window.SB_AUTH_REDIRECT_URL || "https://beta.signal-bench.com/app.html";
+window.SB_AUTH_REDIRECT_URL = window.SB_AUTH_REDIRECT_URL || "https://beta.signal-bench.com/auth.html";
